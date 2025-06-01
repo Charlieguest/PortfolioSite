822ff2cbe75b19cb5950b2a0a58fc3f64e43da48
Charlieguest <114992920+Charlieguest@users.noreply.github.com>
2025-06-01 00:56:51 +0000

    Burger menu implementation

---
+++ assets/js/nav.js
@@ -0,0 +1,15 @@
+const primaryNav = document.querySelector(".nav-bar");
+const navToggle = document.querySelector(".nav-bar__toggle");
+
+navToggle.addEventListener("click", () => {
+    const visibility = primaryNav.getAttribute('data-visible');
+
+    if (visibility === "false") {
+        primaryNav.setAttribute("data-visible", true);
+        navToggle.setAttribute("aria-expanded", true);
+    }
+    else {
+        primaryNav.setAttribute("data-visible", false);
+        navToggle.setAttribute("aria-expanded", false);
+    }
+})
